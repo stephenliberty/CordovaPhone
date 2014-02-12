a6229a66c127898ebfe49e74f04c535dc410f495
Liberty <libertst@BOT-LIBERTST-MAC.local>
2014-02-11 16:21:58 -0800

    tweak

--- src/android/Phone.java
+++ src/android/Phone.java
@@ -1,36 +1,37 @@
 package org.vertafore.cordova;
 
-    import org.apache.cordova.CordovaPlugin;
-    import org.apache.cordova.CallbackContext;
-    import android.telephony.TelephonyManager;
-    import org.json.JSONArray;
-    import org.json.JSONException;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import android.telephony.TelephonyManager;
+import org.json.JSONArray;
+import org.json.JSONException;
 import android.content.Context;
 import java.util.List;
-    import android.content.Intent;
+import android.content.Intent;
 import android.net.Uri;
 import android.content.pm.ResolveInfo;
+import android.content.pm.PackageManager;
 
-    /**
-     * This class echoes a string called from JavaScript.
-     */
-    public class Phone extends CordovaPlugin {
-
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-            if(action.equals("canDevicePlaceAPhoneCall") != true) {
-                return false;
-            }
-            Intent callIntent = new Intent(Intent.ACTION_CALL);
-            callIntent.setData(Uri.parse("tel:5551231234"));
-            List<ResolveInfo> callAppsList = this.cordova.getActivity().getApplicationContext().getPackageManager().queryIntentActivities(callIntent, 0);
-            System.out.println(callAppsList.size());
-            System.out.println(callAppsList);
-            if(callAppsList.size() > 0) {
-                callbackContext.success();
-            } else {
-                callbackContext.error("No handler");
-            }
-            return true;
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class Phone extends CordovaPlugin {
+    
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if(action.equals("canDevicePlaceAPhoneCall") != true) {
+            return false;
         }
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        callIntent.setData(Uri.parse("tel:5551231234"));
+        List<ResolveInfo> callAppsList = this.cordova.getActivity().getApplicationContext().getPackageManager().queryIntentActivities(callIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        System.out.println(callAppsList.size());
+        System.out.println(callAppsList);
+        if(callAppsList.size() > 0) {
+            callbackContext.success();
+        } else {
+            callbackContext.error("No handler");
+        }
+        return true;
     }
+}
